1e6e9b84718c45e6e96d8e3c2237be92967097e1
haru <88703915+lips85@users.noreply.github.com>
2024-07-16 16:46:59 +0900

    2번째 과제 미리 준비!

--- lib/features/00_menu_screen.dart/menu_screen.dart
+++ lib/features/00_menu_screen.dart/menu_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hary_animation/features/01_implicit_animations/implicit_animations_screen.dart';
+import 'package:hary_animation/features/02_explicit_animation/explicit_animation_screen.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
@@ -27,6 +28,13 @@ class MenuScreen extends StatelessWidget {
                   _goToPage(context, const ImplicitAnimationsScreen()),
               child: const Text(
                 "Implicit Animations",
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () =>
+                  _goToPage(context, const ExplicitAnimationScreen()),
+              child: const Text(
+                "Explicit Animations",
               ),
             )
           ],
