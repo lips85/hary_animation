4ad8cd61712670557ced1c5ae4dc680ccdef7d44
haru <godjaewook@naver.com>
2024-07-18 07:18:00 +0900

    확인

--- lib/features/02_explicit_animation/explicit_animation_screen.dart
+++ lib/features/02_explicit_animation/explicit_animation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 class ExplicitAnimationScreen extends StatefulWidget {
   const ExplicitAnimationScreen({super.key});
@@ -8,7 +9,24 @@ class ExplicitAnimationScreen extends StatefulWidget {
       _ExplicitAnimationScreenState();
 }
 
-class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen> {
+class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+  );
+
+  void _play() {
+    _animationController.forward();
+  }
+
+  void _pause() {
+    _animationController.stop();
+  }
+
+  void _reverse() {
+    _animationController.reverse();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +34,32 @@ class _ExplicitAnimationScreenState extends State<ExplicitAnimationScreen> {
       appBar: AppBar(
         title: const Text("Explicit Animations"),
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5,
+              ),
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return Center(
+                  child: AnimatedContainer(
+                    width: MediaQuery.of(context).size.width * 0.1,
+                    height: MediaQuery.of(context).size.width * 0.1,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.easeInOut,
+                    decoration: BoxDecoration(
+                      color: Colors.primaries[index % Colors.primaries.length],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
