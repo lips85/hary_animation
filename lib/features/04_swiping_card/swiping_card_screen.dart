60f11a1de7ba517962a811ff7f0cc1a070eb9d4f
haru <godjaewook@naver.com>
2024-07-24 01:30:12 +0900

    일단 저장

--- lib/features/04_swiping_card/swiping_card_screen.dart
+++ lib/features/04_swiping_card/swiping_card_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:hary_animation/features/04_swiping_card/widgets/card.dart';
 
@@ -11,108 +10,207 @@ class SwipingCard2sScreen extends StatefulWidget {
 }
 
 class _SwipingCard2sScreenState extends State<SwipingCard2sScreen>
-    with SingleTickerProviderStateMixin {
-  late final size = MediaQuery.of(context).size;
+    with TickerProviderStateMixin {
+  late final AnimationController _positionController;
+  late final AnimationController _flipController;
+  late final Animation<double> _flipAnimation;
+  late final Tween<double> _rotation;
+  late final Tween<double> _scale;
 
-  late final AnimationController _position = AnimationController(
-    vsync: this,
-    duration: const Duration(seconds: 1),
-    lowerBound: (size.width + 100) * -1,
-    upperBound: (size.width + 100),
-    value: 0.0,
-  );
+  Color _backgroundColor = Colors.blue.shade300;
 
-  late final Tween<double> _rotation = Tween(
-    begin: -15,
-    end: 15,
-  );
+  int _index = 1;
+  bool _isInitialized = false;
 
-  late final Tween<double> _scale = Tween(
-    begin: 0.8,
-    end: 1,
-  );
+  @override
+  void initState() {
+    super.initState();
+
+    _flipController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+
+    _flipAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(CurvedAnimation(
+      parent: _flipController,
+      curve: Curves.easeInOut,
+    ));
+
+    _rotation = Tween(begin: -15, end: 15);
+    _scale = Tween(begin: 0.8, end: 1);
+  }
+
+  void _initializeControllers(BuildContext context) {
+    if (!_isInitialized) {
+      final size = MediaQuery.of(context).size;
+
+      _positionController = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 1),
+        lowerBound: (size.width + 100) * -1,
+        upperBound: (size.width + 100),
+        value: 0.0,
+      );
+
+      _isInitialized = true;
+    }
+  }
+
+  void _updateBackgroundColor() {
+    setState(() {
+      if (_positionController.value > 0) {
+        _backgroundColor = Colors.green.shade300;
+      } else if (_positionController.value < 0) {
+        _backgroundColor = Colors.red.shade300;
+      } else {
+        _backgroundColor = Colors.blue.shade300;
+      }
+    });
+  }
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
-    _position.value += details.delta.dx;
+    _positionController.value += details.delta.dx;
+    _updateBackgroundColor();
   }
 
   void _whenComplete() {
-    _position.value = 0;
+    _positionController.value = 0;
     setState(() {
       _index = _index == 5 ? 1 : _index + 1;
+      _backgroundColor = Colors.blue.shade300;
     });
   }
 
   void _onHorizontalDragEnd(DragEndDetails details) {
+    final size = MediaQuery.of(context).size;
     final bound = size.width - 200;
     final dropZone = size.width + 100;
-    if (_position.value.abs() >= bound) {
-      if (_position.value.isNegative) {
-        _position.animateTo((dropZone) * -1).whenComplete(_whenComplete);
+    if (_positionController.value.abs() >= bound) {
+      if (_positionController.value.isNegative) {
+        _positionController
+            .animateTo((dropZone) * -1)
+            .whenComplete(_whenComplete);
       } else {
-        _position.animateTo(dropZone).whenComplete(_whenComplete);
+        _positionController.animateTo(dropZone).whenComplete(_whenComplete);
       }
     } else {
-      _position.animateTo(
-        0,
-        curve: Curves.easeOut,
-      );
+      _positionController
+          .animateTo(
+            0,
+            curve: Curves.easeOut,
+          )
+          .whenComplete(_whenComplete);
+    }
+  }
+
+  void _flipCard() {
+    if (_flipController.isCompleted) {
+      _flipController.reverse();
+    } else {
+      _flipController.forward();
     }
   }
 
   @override
   void dispose() {
-    _position.dispose();
+    if (_isInitialized) {
+      _positionController.dispose();
+    }
+    _flipController.dispose();
     super.dispose();
   }
 
-  int _index = 1;
-
   @override
   Widget build(BuildContext context) {
+    _initializeControllers(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Swiping Card2s'),
       ),
-      body: AnimatedBuilder(
-        animation: _position,
-        builder: (context, child) {
-          final angle = _rotation.transform(
-                (_position.value + size.width / 2) / size.width,
-              ) *
-              pi /
-              180;
-          final scale = _scale.transform(_position.value.abs() / size.width);
-          return Stack(
-            alignment: Alignment.topCenter,
-            children: [
-              Positioned(
-                top: 100,
-                child: Transform.scale(
-                  scale: scale,
-                  child: Cards(index: _index == 5 ? 1 : _index + 1),
-                ),
-              ),
-              Positioned(
-                top: 100,
-                child: GestureDetector(
-                  onHorizontalDragUpdate: _onHorizontalDragUpdate,
-                  onHorizontalDragEnd: _onHorizontalDragEnd,
-                  child: Transform.translate(
-                    offset: Offset(_position.value, 0),
-                    child: Transform.rotate(
-                      angle: angle,
-                      child: Cards(
-                        index: _index,
+      body: _isInitialized
+          ? AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              color: _backgroundColor,
+              child: AnimatedBuilder(
+                animation: _positionController,
+                builder: (context, child) {
+                  final angle = _rotation.transform(
+                        (_positionController.value +
+                                MediaQuery.of(context).size.width / 2) /
+                            MediaQuery.of(context).size.width,
+                      ) *
+                      pi /
+                      180;
+                  final scale = _scale.transform(
+                      _positionController.value.abs() /
+                          MediaQuery.of(context).size.width);
+                  return Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      Positioned(
+                        top: 100,
+                        child: Transform.scale(
+                          scale: scale,
+                          child: Cards(index: _index == 5 ? 1 : _index + 1),
+                        ),
                       ),
-                    ),
-                  ),
-                ),
+                      Positioned(
+                        top: 100,
+                        child: GestureDetector(
+                          onTap: _flipCard,
+                          onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                          onHorizontalDragEnd: _onHorizontalDragEnd,
+                          child: AnimatedBuilder(
+                            animation: _flipAnimation,
+                            builder: (context, child) {
+                              final flipAngle = _flipAnimation.value * pi;
+                              return Transform(
+                                transform: Matrix4.identity()
+                                  ..setEntry(3, 2, 0.001)
+                                  ..rotateY(flipAngle),
+                                alignment: Alignment.center,
+                                child: _flipAnimation.value <= 0.5
+                                    ? Transform.translate(
+                                        offset: Offset(
+                                            _positionController.value, 0),
+                                        child: Transform.rotate(
+                                          angle: angle,
+                                          child: Cards(
+                                            index: _index,
+                                          ),
+                                        ),
+                                      )
+                                    : Transform(
+                                        transform: Matrix4.identity()
+                                          ..rotateY(pi),
+                                        alignment: Alignment.center,
+                                        child: Transform.translate(
+                                          offset: Offset(
+                                              _positionController.value, 0),
+                                          child: Transform.rotate(
+                                            angle: angle,
+                                            child: Cards(
+                                              index:
+                                                  _index, // Flip한 후에도 같은 카드로 유지
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
               ),
-            ],
-          );
-        },
-      ),
+            )
+          : Container(),
     );
   }
 }
